d895e6cb2c0e1306bf93ea8c62065b6c8749ecf2
Emran <emranbd77@gmail.com>
2023-06-27 09:00:58 +0600

    dnd drag and drop

--- src/dndkit/Board.tsx
+++ src/dndkit/Board.tsx
@@ -1,10 +1,31 @@
-import { DndContext } from '@dnd-kit/core';
-import React from 'react';
+import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
+import React, { useState } from 'react';
 
 const Board = () => {
+  const [isDroped, setIsDroped] = useState(false);
+  const { isOver, setNodeRef: dropNode } = useDroppable({
+    id: 'dropable',
+  });
+
+  const handleDrapEvent = (event: any) => {
+    if (event.over && event.over.id === 'dropable') {
+      setIsDroped(true);
+    }
+  };
+  const { setNodeRef: dragNode, listeners } = useDraggable({
+    id: 'dragable',
+  });
   return (
-    <DndContext>
-      <h1>hello world</h1>
+    <DndContext onDragEnd={handleDrapEvent}>
+      <button ref={dragNode} type="button" className="p-2 bg-orange-400">
+        Drag
+      </button>
+
+      <div
+        ref={dropNode}
+        {...listeners}
+        className="drop-container w-32 h-32 bg-red-300"
+      ></div>
     </DndContext>
   );
 };
