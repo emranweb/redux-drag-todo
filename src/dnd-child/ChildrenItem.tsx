fd76980ac759ce00de2d4ddd6ec9693445b815f9
emranweb <emranbd77@gmail.com>
2023-08-01 10:44:38 +0600

    item remove function

--- src/dnd-child/ChildrenItem.tsx
+++ src/dnd-child/ChildrenItem.tsx
@@ -2,6 +2,7 @@ import { useSortable } from '@dnd-kit/sortable';
 import React from 'react';
 import { CSS } from '@dnd-kit/utilities';
 import { Handle } from './Handle';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 type ChildItemTypes = {
     id: string;
@@ -19,10 +20,17 @@ const ChildrenItem = ({
     depth,
     handleCollapse,
 }: ChildItemTypes) => {
-    const { transform, attributes, setNodeRef, transition, listeners } =
-        useSortable({
-            id: id,
-        });
+    const {
+        transform,
+        attributes,
+        setNodeRef,
+        transition,
+        listeners,
+        setDraggableNodeRef,
+        setDroppableNodeRef,
+    } = useSortable({
+        id: id,
+    });
 
     const style = { transform: CSS.Transform.toString(transform), transition };
 
@@ -33,21 +41,23 @@ const ChildrenItem = ({
                 marginLeft ? 'left-[40px]' : ''
             }`}
             style={style}
-            ref={setNodeRef}
-            {...listeners}
-            {...attributes}
+            ref={setDroppableNodeRef}
         >
-            <Handle ref={setNodeRef} />
-            {title}
-            <button className="_2KKbCQhSKJyhMsrU01Xnm7 _3B4PJavUN0slKomrs7UBiy _1yuB6IonqthW0XDve0jXJ">
-                <svg
-                    width="10"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 70 41"
-                >
-                    <path d="M30.76 39.2402C31.885 40.3638 33.41 40.995 35 40.995C36.59 40.995 38.115 40.3638 39.24 39.2402L68.24 10.2402C69.2998 9.10284 69.8768 7.59846 69.8494 6.04406C69.822 4.48965 69.1923 3.00657 68.093 1.90726C66.9937 0.807959 65.5106 0.178263 63.9562 0.150837C62.4018 0.123411 60.8974 0.700397 59.76 1.76024L35 26.5102L10.24 1.76024C9.10259 0.700397 7.59822 0.123411 6.04381 0.150837C4.4894 0.178263 3.00632 0.807959 1.90702 1.90726C0.807714 3.00657 0.178019 4.48965 0.150593 6.04406C0.123167 7.59846 0.700153 9.10284 1.75999 10.2402L30.76 39.2402Z"></path>
-                </svg>
-            </button>
+            <div
+                ref={setDraggableNodeRef}
+                className="flex items-center justify-between w-full"
+            >
+                {/* <span onClick={() => console.log('hi')}>Remove</span> */}
+                <Handle
+                    ref={setDraggableNodeRef}
+                    {...listeners}
+                    {...attributes}
+                />
+                {title}
+                <span onClick={() => console.log('hi')}>
+                    <AiOutlineCloseCircle />
+                </span>
+            </div>
         </div>
     );
 };
