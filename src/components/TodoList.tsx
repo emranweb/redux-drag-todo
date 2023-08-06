8e30cc8218e3dc71d71ba9fab0ffdc591d353838
emranweb <emranbd77@gmail.com>
2023-08-06 20:23:09 +0600

    fix the eslint error

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,13 +8,13 @@ import { Todo } from '../types';
 // import { fetchPost } from '../redux/rtk-todo/rtk-fetch';
 
 const TodoList = ({ todos }: { todos: Todo[] }) => {
-  return (
-    <div className="mt-2 text-gray-700 text-sm h-32">
-      {todos.map(item => (
-        <TodoItem key={item.id} todo={item} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="mt-2 text-gray-700 text-sm h-32">
+            {todos.map(item => (
+                <TodoItem key={item.id} todo={item} />
+            ))}
+        </div>
+    );
 };
 
 export default TodoList;
