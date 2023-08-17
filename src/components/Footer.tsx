5628a0a19e50ffab0b2ec5ddd5eb96d638f0a6e3
Emran <emranbd77@gmail.com>
2023-08-17 11:56:37 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="w-full text-gray-500 mt-32 py-4 text-center">
             <p>
                 Copyright &copy; 2023 by
-                <a href="https://github.com/emranweb">emranweb</a>
+                <a href="https://github.com/emranweb"> emranweb</a>
             </p>
         </div>
     );
